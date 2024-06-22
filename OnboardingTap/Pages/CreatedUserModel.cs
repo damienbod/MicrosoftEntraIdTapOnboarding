e4001f4d4fc3315552a86afc53dd7aeb4c3ad055
damienbod <damien_bod@hotmail.com>
2024-06-22 18:25:11 +0200

    update

--- OnboardingTap/Pages/CreatedUserModel.cs
+++ OnboardingTap/Pages/CreatedUserModel.cs
@@ -1,11 +1,10 @@
-﻿namespace OnboardingTap.Pages
+﻿namespace OnboardingTap.Pages;
+
+public class CreatedUserModel
 {
-    public class CreatedUserModel
-    {
-        public string Email { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
 
-        public string Id { get; set; } = string.Empty;
+    public string Id { get; set; } = string.Empty;
 
-        public string Password { get; set; } = string.Empty;
-    }
+    public string Password { get; set; } = string.Empty;
 }
